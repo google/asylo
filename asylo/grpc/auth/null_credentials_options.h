7072756a5b969103b00d45033b3ddc47ba184bf8
Anna Sapek <asapek@google.com>
2018-06-05 11:41:52 -0700

    Add factory functions for enclave credentials opts
    This commit adds functions for creating options used to configure
    enclave gRPC credentials. Currently, there are two types of credentials
    options supported:
      * Null credentials options for a bidirectionally-unauthenticated
        channel
      * SGX local credentials options for a channel with
        bidirectional authentication based on SGX code identity
    
    This is part of a cleanup to hide internals of the identity library from
    Asylo users. Asylo users should configure their gRPC credentials using
    the public options factories provided in the asylo/grpc/auth package.
    
    PiperOrigin-RevId: 199332476

---
+++ asylo/grpc/auth/null_credentials_options.h
@@ -0,0 +1,54 @@
+/*
+ *
+ * Copyright 2018 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef ASYLO_GRPC_AUTH_NULL_CREDENTIALS_OPTIONS_H_
+#define ASYLO_GRPC_AUTH_NULL_CREDENTIALS_OPTIONS_H_
+
+#include "asylo/grpc/auth/enclave_credentials_options.h"
+
+namespace asylo {
+
+/// Creates options suitable for configuring a credential used in establishing a
+/// bidirectionally-unauthenticated gRPC channel between two enclave entities.
+///
+/// A credential configured with these options enforces bidirectional
+/// authentication using the null identity. The null identity specifies no
+/// identity in particular, which means that the resulting connection is
+/// essentially unauthenticated.
+///
+/// Sample usage for creating `grpc::ChannelCredentials`:
+///
+/// ```
+/// std::shared_ptr<grpc::ChannelCredentials> creds =
+///   EnclaveChannelCredentials(BidirectionalNullCredentialsOptions());
+/// ```
+///
+/// Sample usage for creating `grpc::ServerCredentials`:
+///
+/// ```
+/// std::shared_ptr<grpc::ServerCredentials> creds =
+///   EnclaveServerCredentials(BidirectionalNullCredentialsOptions());
+/// ```
+///
+/// \return Options used to configure gRPC credentials for a
+///         bidirectionally-unauthenticated channel.
+EnclaveCredentialsOptions BidirectionalNullCredentialsOptions();
+
+}  // namespace asylo
+
+#endif  // ASYLO_GRPC_AUTH_NULL_CREDENTIALS_OPTIONS_H_
---
+++ asylo/grpc/auth/sgx_credentials_options.h
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright 2018 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef ASYLO_GRPC_AUTH_SGX_CREDENTIALS_OPTIONS_H_
+#define ASYLO_GRPC_AUTH_SGX_CREDENTIALS_OPTIONS_H_
+
+#include "asylo/grpc/auth/enclave_credentials_options.h"
+
+namespace asylo {
+
+/// Creates options suitable for configuring a credential used in establishing a
+/// bidirectionally-authenticated gRPC channel between two local SGX enclaves.
+///
+/// A credential configured with these options enforces bidirectional
+/// authentication using SGX enclave code identity.
+///
+/// Sample usage for creating `grpc::ChannelCredentials`:
+///
+/// ```
+/// std::shared_ptr<grpc::ChannelCredentials> creds =
+///   EnclaveChannelCredentials(BidirectionalSgxLocalCredentialsOptions());
+/// ```
+///
+/// Sample usage for creating `grpc::ServerCredentials`:
+///
+/// ```
+/// std::shared_ptr<grpc::ServerCredentials> creds =
+///   EnclaveServerCredentials(BidirectionalSgxLocalCredentialsOptions());
+/// ```
+///
+/// \return Options used to configure gRPC credentials for a
+///         bidirectionally-authenticated channel between SGX enclaves on the
+///         same platform.
+EnclaveCredentialsOptions BidirectionalSgxLocalCredentialsOptions();
+
+}  // namespace asylo
+
+#endif  // ASYLO_GRPC_AUTH_SGX_CREDENTIALS_OPTIONS_H_
