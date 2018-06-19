c404617b114d7d34eafa9e028ac2f8354e2f3d5e
Anna Sapek <asapek@google.com>
2018-06-18 17:57:14 -0700

    Add a test for gRPC between two enclaves
    This change adds a new test that runs two enclaves: one hosts a gRPC
    server and the other uses a gRPC client to communicate with a gRPC
    server. The test verifies that the gRPC client can form a secure channel
    to the gRPC server based on SGX local attestation, and make an RPC.
    
    This test will be reused in the future to test additional attestation
    mechanisms and gRPC credentials configurations.
    
    PiperOrigin-RevId: 201092697

---
+++ asylo/test/grpc/client_enclave.cc
@@ -0,0 +1,80 @@
+/*
+ *
+ * Copyright 2018 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <memory>
+#include <string>
+
+#include "absl/time/time.h"
+#include "asylo/enclave.pb.h"
+#include "asylo/grpc/auth/enclave_channel_credentials.h"
+#include "asylo/grpc/auth/sgx_credentials_options.h"
+#include "asylo/test/grpc/client_enclave.pb.h"
+#include "asylo/test/grpc/messenger_client_impl.h"
+#include "asylo/trusted_application.h"
+#include "asylo/util/status.h"
+#include "include/grpc/impl/codegen/gpr_types.h"
+#include "include/grpc/support/time.h"
+
+namespace asylo {
+
+const int64_t kDeadlineMicros = absl::Seconds(10) / absl::Microseconds(1);
+
+// An enclave that makes RPCs to a MessengerServer. Uses SGX local attestation
+// to connect to an enclave hosting a MessengerServer.
+class ClientEnclave : public TrustedApplication {
+ public:
+  Status Run(const EnclaveInput &input, EnclaveOutput *output) override {
+    if (!input.HasExtension(server_address)) {
+      return Status(error::GoogleError::INVALID_ARGUMENT,
+                    "Input missing server_address extension");
+    }
+    if (!input.HasExtension(rpc_input)) {
+      return Status(error::GoogleError::INVALID_ARGUMENT,
+                    "Input missing rpc_input extension");
+    }
+
+    const std::string &address = input.GetExtension(server_address);
+
+    std::shared_ptr<::grpc::ChannelCredentials> channel_credentials =
+        EnclaveChannelCredentials(BidirectionalSgxLocalCredentialsOptions());
+
+    // Connect a gRPC channel to the server specified in the input.
+    std::shared_ptr<::grpc::Channel> channel =
+        ::grpc::CreateChannel(address, channel_credentials);
+    test::MessengerClient1 client(channel);
+    gpr_timespec absolute_deadline =
+        gpr_time_add(gpr_now(GPR_CLOCK_REALTIME),
+                     gpr_time_from_micros(kDeadlineMicros, GPR_TIMESPAN));
+    if (!channel->WaitForConnected(absolute_deadline)) {
+      return Status(error::GoogleError::INTERNAL,
+                    "Failed to connect to server");
+    }
+
+    // Make an RPC to the server and output the response.
+    StatusOr<std::string> result = client.Hello(input.GetExtension(rpc_input));
+    if (!result.ok()) {
+      return result.status();
+    }
+    output->SetExtension(rpc_result, result.ValueOrDie());
+    return Status::OkStatus();
+  }
+};
+
+TrustedApplication *BuildTrustedApplication() { return new ClientEnclave(); }
+
+}  // namespace asylo
---
+++ asylo/test/grpc/enclave_communication_test.cc
@@ -0,0 +1,130 @@
+/*
+ *
+ * Copyright 2018 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/str_cat.h"
+#include "asylo/enclave.pb.h"
+#include "asylo/grpc/util/enclave_server.pb.h"
+#include "asylo/test/grpc/client_enclave.pb.h"
+#include "asylo/test/grpc/messenger_server_impl.h"
+#include "asylo/test/util/enclave_test_launcher.h"
+#include "asylo/test/util/status_matchers.h"
+#include "asylo/test/util/test_flags.h"
+#include "asylo/util/status.h"
+
+namespace asylo {
+namespace {
+
+DEFINE_string(client_enclave_path, "", "Path to client enclave");
+DEFINE_string(server_enclave_path, "", "Path to server enclave");
+
+constexpr char kName[] = "Mellanie Rescorai";
+constexpr char kLocalAttestationDomain[] = "A unique attestation domain";
+
+// A test for gRPC communication between two enclaves.
+//
+// This test expects the enclave paths to be passed in through the
+// --client_enclave_path and --server_enclave_path flags.
+class EnclaveCommunicationTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    EnclaveConfig config;
+    if (!FLAGS_test_tmpdir.empty()) {
+      config.mutable_logging_config()->set_log_directory(FLAGS_test_tmpdir);
+    }
+
+    // The client and server must have the same local attestation domain in
+    // order for SGX local attestation to work.
+    config.mutable_host_config()->set_local_attestation_domain(
+        kLocalAttestationDomain);
+
+    ASSERT_THAT(SetUpServer(config), IsOk());
+    ASSERT_THAT(SetUpClient(config), IsOk());
+  }
+
+  Status SetUpServer(EnclaveConfig config) {
+    // Use port auto-selection for the server's address.
+    ServerConfig *server_config = config.MutableExtension(server_input_config);
+    server_config->set_host("[::1]");
+    server_config->set_port(0);
+
+    Status status = server_launcher_.SetUp(FLAGS_server_enclave_path, config,
+                                           "/grpc/server");
+    if (!status.ok()) {
+      return status;
+    }
+
+    // Initialize the gRPC server and get its address.
+    EnclaveInput input;
+    input.SetExtension(command, ServerCommand::INITIALIZE_SERVER);
+    EnclaveOutput output;
+    status = server_launcher_.mutable_client()->EnterAndRun(input, &output);
+    if (!status.ok()) {
+      return status;
+    }
+
+    const ServerConfig &final_server_config =
+        output.GetExtension(server_output_config);
+    server_address_ = absl::StrCat(final_server_config.host(), ":",
+                                   final_server_config.port());
+
+    return Status::OkStatus();
+  }
+
+  Status SetUpClient(const EnclaveConfig &config) {
+    Status status = client_launcher_.SetUp(FLAGS_client_enclave_path, config,
+                                           "/grpc/client");
+    if (!status.ok()) {
+      return status;
+    }
+
+    grpc_client_enclave_ = client_launcher_.mutable_client();
+    return Status::OkStatus();
+  }
+
+  void TearDown() override {
+    EnclaveFinal enclave_final;
+    // Finalize both enclaves, even if tear down fails for the first one.
+    EXPECT_THAT(
+        client_launcher_.TearDown(enclave_final, /*skipTearDown=*/false),
+        IsOk());
+    EXPECT_THAT(
+        server_launcher_.TearDown(enclave_final, /*skipTearDown=*/false),
+        IsOk());
+  }
+
+  EnclaveTestLauncher client_launcher_;
+  EnclaveTestLauncher server_launcher_;
+  EnclaveClient *grpc_client_enclave_;
+  std::string server_address_;
+};
+
+TEST_F(EnclaveCommunicationTest, SimpleSynchronousRpc) {
+  EnclaveInput input;
+  input.SetExtension(server_address, server_address_);
+  input.SetExtension(rpc_input, kName);
+  EnclaveOutput output;
+  ASSERT_THAT(grpc_client_enclave_->EnterAndRun(input, &output), IsOk());
+  EXPECT_EQ(output.GetExtension(rpc_result),
+            test::MessengerServer1::ResponseString(kName));
+}
+
+
+}  // namespace
+}  // namespace asylo
