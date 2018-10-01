33c6f8e643c5fe6dad00261d5bf99a5910127748
Matt Gingell <gingell@google.com>
2018-10-01 13:10:29 -0700

    Move error codes into own header file
    PiperOrigin-RevId: 215266818

---
+++ asylo/util/error_codes.h
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright 2017 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef ASYLO_UTIL_ERROR_CODES_H_
+#define ASYLO_UTIL_ERROR_CODES_H_
+
+namespace asylo {
+namespace error {
+
+/// This file defines the enum associated with the Google canonical error
+/// space. Note that this file may be included by low-level runtime components
+/// which must compile without C++ runtime support. Dependencies which require
+/// should be added in error_space.h itself, and this file should declare the
+/// canonical error code enumeration only.
+enum GoogleError : int {
+  OK = 0,
+  CANCELLED = 1,
+  UNKNOWN = 2,
+  INVALID_ARGUMENT = 3,
+  DEADLINE_EXCEEDED = 4,
+  NOT_FOUND = 5,
+  ALREADY_EXISTS = 6,
+  PERMISSION_DENIED = 7,
+  RESOURCE_EXHAUSTED = 8,
+  FAILED_PRECONDITION = 9,
+  ABORTED = 10,
+  OUT_OF_RANGE = 11,
+  UNIMPLEMENTED = 12,
+  INTERNAL = 13,
+  UNAVAILABLE = 14,
+  DATA_LOSS = 15,
+  UNAUTHENTICATED = 16
+};
+
+}  // namespace error
+}  // namespace asylo
+
+#endif  // ASYLO_UTIL_ERROR_CODES_H_
--- asylo/util/error_space.h
+++ asylo/util/error_space.h
@@ -23,6 +23,7 @@
 #include <unordered_map>
 
 #include "asylo/platform/common/static_map.h"
+#include "asylo/util/error_codes.h"
 
 // This file, along with the associated cc file, defines the basic
 // infrastructure for error-spaces that could be used with the
@@ -115,27 +116,6 @@ struct error_enum_traits {
   }
 };
 /// \endcond
-
-/// Defines the enum associated with the Google canonical error space.
-enum GoogleError : int {
-  OK = 0,
-  CANCELLED = 1,
-  UNKNOWN = 2,
-  INVALID_ARGUMENT = 3,
-  DEADLINE_EXCEEDED = 4,
-  NOT_FOUND = 5,
-  ALREADY_EXISTS = 6,
-  PERMISSION_DENIED = 7,
-  RESOURCE_EXHAUSTED = 8,
-  FAILED_PRECONDITION = 9,
-  ABORTED = 10,
-  OUT_OF_RANGE = 11,
-  UNIMPLEMENTED = 12,
-  INTERNAL = 13,
-  UNAVAILABLE = 14,
-  DATA_LOSS = 15,
-  UNAUTHENTICATED = 16
-};
 
 /// All implementations of error spaces are derived from this abstract class.
 /// \related GoogleErrorSpace
