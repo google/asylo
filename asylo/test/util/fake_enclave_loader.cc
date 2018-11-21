a11698effb14ff98adf3596ee7d6c8c22a2da813
Conrad Parker <conradparker@google.com>
2018-11-21 12:27:29 -0800

    Add a FakeEnclaveLoader
    FakeEnclaveLoader loads an EnclaveClient provided in the constructor.
    
    Together with MockEnclaveClient, this allows users to test
    load-run-destroy workflows with mocks.
    
    PiperOrigin-RevId: 222445396

---
+++ asylo/test/util/fake_enclave_loader.cc
@@ -0,0 +1,32 @@
+/*
+ *
+ * Copyright 2018 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "asylo/test/util/fake_enclave_loader.h"
+
+namespace asylo {
+
+FakeEnclaveLoader::FakeEnclaveLoader(
+    std::unique_ptr<EnclaveClient> destination_client)
+    : client_(std::move(destination_client)) {}
+
+StatusOr<std::unique_ptr<EnclaveClient>> FakeEnclaveLoader::LoadEnclave(
+    const std::string &name, void *base_address, const EnclaveConfig &config) const {
+  return std::move(client_);
+}
+
+}  // namespace asylo
---
+++ asylo/test/util/fake_enclave_loader.h
@@ -0,0 +1,50 @@
+/*
+ *
+ * Copyright 2018 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef ASYLO_TEST_UTIL_FAKE_ENCLAVE_LOADER_H_
+#define ASYLO_TEST_UTIL_FAKE_ENCLAVE_LOADER_H_
+
+#include <memory>
+
+#include "asylo/client.h"
+#include "asylo/enclave_manager.h"
+#include "asylo/util/statusor.h"
+
+namespace asylo {
+
+/// A fake `EnclaveLoader` that loads a given `EnclaveClient`.
+class FakeEnclaveLoader : public EnclaveLoader {
+ public:
+  /// Creates a `FakeEnclaveLoader` that will load a given client.
+  ///
+  /// \param client The `EnclaveClient` to be loaded.
+  explicit FakeEnclaveLoader(std::unique_ptr<EnclaveClient> client);
+
+ private:
+  // From EnclaveLoader.
+  StatusOr<std::unique_ptr<EnclaveClient>> LoadEnclave(
+      const std::string &name, void *base_address,
+      const EnclaveConfig &config) const override;
+
+  // The client to be loaded.
+  mutable std::unique_ptr<EnclaveClient> client_;
+};
+
+}  // namespace asylo
+
+#endif  // ASYLO_TEST_UTIL_FAKE_ENCLAVE_LOADER_H_
