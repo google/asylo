179d501baaf5965e2f9c163b8e6e71fece263a86
Carmen Hanish <cyhanish@google.com>
2019-02-14 16:11:15 -0800

    Add doxygen documentation to AeadCryptor
    Adds doxygen documentation to AeadCryptor because its visibility
    is public.
    
    PiperOrigin-RevId: 234048530

--- asylo/crypto/aead_cryptor.h
+++ asylo/crypto/aead_cryptor.h
@@ -32,59 +32,102 @@
 namespace asylo {
 namespace experimental {
 
-// An AEAD cryptor that provides Seal() and Open() functionality. Currently
-// supported configurations:
-// * AES-GCM-128 and AES-GCM-256 with 96-bit random nonces.
-// * AES-GCM-SIV-128 and AES-GCM-SIV-256 with 96-bit random nonces.
+/// An AEAD cryptor that provides Seal() and Open() functionality. Currently
+/// supported configurations:
+/// * AES-GCM-128 and AES-GCM-256 with 96-bit random nonces.
+/// * AES-GCM-SIV-128 and AES-GCM-SIV-256 with 96-bit random nonces. (For
+///   information on AES-GCM-SIV see https://cyber.biu.ac.il/aes-gcm-siv/)
 class AeadCryptor {
  public:
-  // Creates an AeadCryptor that uses AES-GCM for Seal() and Open(), and
-  // generates random 96-bit nonces for use in Seal().
+  /// Creates a cryptor that uses AES-GCM for Seal() and Open(), and generates
+  /// random 96-bit nonces for use in Seal().
+  ///
+  /// \param key The underlying key used for encryption and decryption.
+  /// \return A pointer to the created cryptor, or a non-OK Status if creation
+  ///         failed.
   static StatusOr<std::unique_ptr<AeadCryptor>> CreateAesGcmCryptor(
       ByteContainerView key);
 
-  // Creates an AeadCryptor that uses AES-GCM-SIV for Seal() and Open(), and
-  // generates random 96-bit nonces for use in Seal().
+  /// Creates a cryptor that uses AES-GCM-SIV for Seal() and Open(), and
+  /// generates random 96-bit nonces for use in Seal().
+  ///
+  /// \param key The underlying key used for encryption and decryption.
+  /// \return A pointer to the created cryptor, or a non-OK Status if creation
+  ///         failed.
   static StatusOr<std::unique_ptr<AeadCryptor>> CreateAesGcmSivCryptor(
       ByteContainerView key);
 
-  // Returns the maximum size of a message that may be sealed successfully with
-  // a cryptor that uses |scheme|.
+  /// Gets the maximum size of a message that may be sealed successfully with a
+  /// cryptor that uses `scheme`.
+  ///
+  /// \param scheme The associated AeadScheme.
+  /// \return The maximum message size that may be sealed successfully, or a
+  ///         non-OK Status if `scheme` is unsupported.
   static StatusOr<size_t> MaxMessageSize(AeadScheme scheme);
 
-  // Returns the maximum number of messages that may be sealed successfully with
-  // a cryptor that uses |scheme|.
+  /// Gets the maximum number of messages that may be sealed safely with a
+  /// cryptor that uses `scheme`.
+  ///
+  /// \param scheme The associated AeadScheme.
+  /// \return The maximum number of messages that may be sealed safely, or a
+  ///         non-OK Status if `scheme` is unsupported.
   static StatusOr<uint64_t> MaxSealedMessages(AeadScheme scheme);
 
-  // Returns the maximum size of a message that may be sealed successfully.
+  /// Gets the maximum size of a message that may be sealed successfully.
+  ///
+  /// \return The maximum message size that this cryptor will seal successfully.
   size_t MaxMessageSize() const;
 
-  // Returns the maximum number of messages that may be sealed successfully.
+  /// Gets the maximum number of messages that may be sealed successfully.
+  ///
+  /// \return The maximum number of messages that this cryptor will seal
+  ///         successfully.
   uint64_t MaxSealedMessages() const;
 
-  // Returns the max overhead of Seal().
+  /// Gets the max overhead of Seal().
+  ///
+  /// \return The maximum space overhead of Seal().
   size_t MaxSealOverhead() const;
 
-  // Returns the nonce size.
+  /// Gets the nonce size.
+  ///
+  /// \return The nonce size.
   size_t NonceSize() const;
 
-  // Implements the AEAD Seal operation. The nonce used is returned through
-  // |nonce| and the authenticated ciphertext is written to |ciphertext|.
-  // |plaintext|.size() must be less than or equal to MaxMessageSize().
-  // |nonce|.size() must be greater than or equal to the value returned by
-  // NonceSize(). |ciphertext|.size() must be greater than or equal to
-  // |plaintext|.size() + MaxSealOverhead(). |ciphertext| is not resized, but
-  // its final size is returned through |ciphertext_size|. Seal() will succeed
-  // at most MaxSealedMessages() times.
+  /// Implements the AEAD Seal operation.
+  ///
+  /// The nonce used is returned through `nonce` and the authenticated
+  /// ciphertext is written to `ciphertext`. `plaintext.size()` must be less
+  /// than or equal to MaxMessageSize(). `nonce.size()` must be greater than or
+  /// equal to the value returned by NonceSize(). `ciphertext.size()` must be
+  /// greater than or equal to `plaintext.size()` + MaxSealOverhead().
+  /// `ciphertext` is not resized, but its final size is returned through
+  /// `ciphertext_size`. Seal() will succeed at most MaxSealedMessages() times.
+  ///
+  /// \param plaintext The secret that will be sealed.
+  /// \param associated_data The authenticated data for the Seal() operation.
+  /// \param[out] nonce The generated nonce.
+  /// \param[out] ciphertext The sealed ciphertext of `plaintext`.
+  /// \param[out] ciphertext_size The size of `ciphertext`.
+  /// \return The resulting status of the Seal() operation.
   Status Seal(ByteContainerView plaintext, ByteContainerView associated_data,
               absl::Span<uint8_t> nonce, absl::Span<uint8_t> ciphertext,
               size_t *ciphertext_size);
 
-  // Implements the AEAD Open operation. |nonce|.size() must be greater than or
-  // equal to the value returned by NonceSize(). |plaintext| is not resized, but
-  // its final size is returned through |plaintext_size|. To ascertain that
-  // |plaintext| is not smaller than is necessary for Open(), |plaintext|.size()
-  // should be greater than or equal to |ciphertext|.size().
+  /// Implements the AEAD Open operation.
+  ///
+  /// `nonce.size()` must be greater than or equal to the value returned by
+  /// NonceSize(). `plaintext` is not resized, but its final size is returned
+  /// through `plaintext_size`. To ascertain that `plaintext` is not smaller
+  /// than is necessary for Open(), `plaintext.size()` should be greater than or
+  /// equal to `ciphertext.size()`.
+  ///
+  /// \param ciphertext The sealed ciphertext.
+  /// \param associated_data The authenticated data for the Open() operation.
+  /// \param nonce The nonce used to seal the ciphertext.
+  /// \param[out] plaintext The unsealed ciphertext.
+  /// \param[out] plaintext_size The size of the plaintext.
+  /// \return The resulting status of the Open() operation.
   Status Open(ByteContainerView ciphertext, ByteContainerView associated_data,
               ByteContainerView nonce, absl::Span<uint8_t> plaintext,
               size_t *plaintext_size);
