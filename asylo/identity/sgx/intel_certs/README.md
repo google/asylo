7292f3e5369f7893c46ab89d497188508231f05d
Carmen Hanish <cyhanish@google.com>
2019-05-07 09:53:33 -0700

    Add Intel SGX root certificate
    Adds Intel SGX root certificate for uses
    in remote attestation.
    
    PiperOrigin-RevId: 247037831
    Change-Id: Ie8834336aa81778d7f44d8b56a7f419aa112a076

---
+++ asylo/identity/sgx/intel_certs/README.md
@@ -0,0 +1,14 @@
+# intel_certs
+
+Certificates in this directory are included for convenience. Asylo authors make
+no assertions of the accuracy of these certificates. Asylo users should either
+import their own copies of these certificates or verify the accuracy of the
+certificates herein.
+
+## intel_sgx_root_ca_cert
+
+This certificate is the root certificate for certificates from the Intel SGX PCK
+Platform CA and the Intel SGX PCK Processor CA, and the root certificate for the
+Intel SGX TCB Signing Certificate. Its use is further discussed in the
+Certificate Hierarchy section of
+[this document](https://download.01.org/intel-sgx/dcap-1.0.1/docs/SGX_PCK_Certificate_CRL_Spec-1.0.pdf).
---
+++ asylo/identity/sgx/intel_certs/intel_sgx_root_ca_cert.cc
@@ -0,0 +1,41 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "asylo/identity/sgx/intel_certs/intel_sgx_root_ca_cert.h"
+
+namespace asylo {
+
+const char *const kIntelSgxRootCaCertificate =
+    R"(-----BEGIN CERTIFICATE-----
+MIICjzCCAjSgAwIBAgIUImUM1lqdNInzg7SVUr9QGzknBqwwCgYIKoZIzj0EAwIw
+aDEaMBgGA1UEAwwRSW50ZWwgU0dYIFJvb3QgQ0ExGjAYBgNVBAoMEUludGVsIENv
+cnBvcmF0aW9uMRQwEgYDVQQHDAtTYW50YSBDbGFyYTELMAkGA1UECAwCQ0ExCzAJ
+BgNVBAYTAlVTMB4XDTE4MDUyMTEwNDExMVoXDTMzMDUyMTEwNDExMFowaDEaMBgG
+A1UEAwwRSW50ZWwgU0dYIFJvb3QgQ0ExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0
+aW9uMRQwEgYDVQQHDAtTYW50YSBDbGFyYTELMAkGA1UECAwCQ0ExCzAJBgNVBAYT
+AlVTMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEC6nEwMDIYZOj/iPWsCzaEKi7
+1OiOSLRFhWGjbnBVJfVnkY4u3IjkDYYL0MxO4mqsyYjlBalTVYxFP2sJBK5zlKOB
+uzCBuDAfBgNVHSMEGDAWgBQiZQzWWp00ifODtJVSv1AbOScGrDBSBgNVHR8ESzBJ
+MEegRaBDhkFodHRwczovL2NlcnRpZmljYXRlcy50cnVzdGVkc2VydmljZXMuaW50
+ZWwuY29tL0ludGVsU0dYUm9vdENBLmNybDAdBgNVHQ4EFgQUImUM1lqdNInzg7SV
+Ur9QGzknBqwwDgYDVR0PAQH/BAQDAgEGMBIGA1UdEwEB/wQIMAYBAf8CAQAwCgYI
+KoZIzj0EAwIDSQAwRgIhAIpQ/KlO1XE4hH8cw5Ol/E0yzs8PToJe9Pclt+bhfLUg
+AiEAss0qf7FlMmAMet+gbpLD97ldYy/wqjjmwN7yHRVr2AM=
+-----END CERTIFICATE-----)";
+
+}  // namespace asylo
