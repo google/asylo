c005b874046762358bd54cbc5ec417a4443c086e
Chong Cai <chongc@google.com>
2019-05-23 15:48:43 -0700

    Add sgx_local_assertion_authority config file
    This is used by fork in SGX hardware mode, needed for Redis to take
    snapshots in SGX hardware mode.
    
    PiperOrigin-RevId: 249731246
    Change-Id: I5f3d2e9fb1f2d486f1d4c268b5347788aa62bbb7

---
+++ asylo/examples/redis/redis_enclave_config.cc
@@ -0,0 +1,44 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "asylo/enclave.pb.h"
+#include "asylo/util/logging.h"
+#include "asylo/identity/descriptions.h"
+#include "asylo/identity/enclave_assertion_authority_config.pb.h"
+#include "asylo/identity/sgx/sgx_local_assertion_authority_config.pb.h"
+
+// The attestation domain is expected to be a 16-byte unique identifier.
+constexpr char kAttestationDomain[] = "A 16-byte string";
+
+extern "C" asylo::EnclaveConfig GetApplicationConfig() {
+  asylo::EnclaveAssertionAuthorityConfig assertion_authority_config;
+  asylo::SetSgxLocalAssertionDescription(
+      assertion_authority_config.mutable_description());
+
+  asylo::SgxLocalAssertionAuthorityConfig sgx_local_assertion_authority_config;
+  sgx_local_assertion_authority_config.set_attestation_domain(
+      kAttestationDomain);
+  CHECK(sgx_local_assertion_authority_config.SerializeToString(
+      assertion_authority_config.mutable_config()));
+
+  asylo::EnclaveConfig enclave_config;
+  *enclave_config.add_enclave_assertion_authority_configs() =
+      std::move(assertion_authority_config);
+
+  return enclave_config;
+}
