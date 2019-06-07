5625fd1478618e6dcca4be894152a3ba357f2e44
Anubhav Srivastava <anubhavsr@google.com>
2019-06-07 11:48:17 -0700

    Add SGX implementation for TestBackend
    PiperOrigin-RevId: 252090271
    Change-Id: Idb725c3e58ac6283b71883d6dbdd511f543193b5

---
+++ asylo/platform/primitives/test/sgx_test_backend.cc
@@ -0,0 +1,45 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "asylo/platform/primitives/test/sgx_test_backend.h"
+
+#include <memory>
+
+#include "absl/memory/memory.h"
+#include "gflags/gflags.h"
+#include "asylo/platform/primitives/sgx/untrusted_sgx.h"
+#include "asylo/platform/primitives/test/test_backend.h"
+#include "asylo/platform/primitives/untrusted_primitives.h"
+#include "asylo/platform/primitives/util/dispatch_table.h"
+#include "asylo/util/status_macros.h"
+#include "asylo/util/statusor.h"
+
+namespace asylo {
+namespace primitives {
+namespace test {
+
+void *loader_config = nullptr;
+
+TestBackend *TestBackend::Get() {
+  static TestBackend *backend = new SgxTestBackend;
+  return backend;
+}
+
+}  // namespace test
+}  // namespace primitives
+}  // namespace asylo
---
+++ asylo/platform/primitives/test/sgx_test_backend.h
@@ -0,0 +1,68 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef ASYLO_PLATFORM_PRIMITIVES_TEST_SGX_TEST_BACKEND_H_
+#define ASYLO_PLATFORM_PRIMITIVES_TEST_SGX_TEST_BACKEND_H_
+
+#include "absl/flags/flag.h"
+#include "asylo/enclave.pb.h"  // IWYU pragma: export
+#include "asylo/platform/primitives/sgx/untrusted_sgx.h"
+#include "asylo/platform/primitives/test/test_backend.h"
+#include "asylo/util/statusor.h"
+
+ABSL_FLAG(std::string, enclave_binary, "",
+          "Path to the SGX enclave binary to be loaded");
+
+namespace asylo {
+namespace primitives {
+namespace test {
+
+extern void *loader_config;
+
+class SgxTestBackend : public TestBackend {
+ public:
+  SgxTestBackend();
+
+  // Loads an instance of a SGX test enclave, aborting on failure.
+  StatusOr<std::shared_ptr<Client>> LoadTestEnclave(
+      const absl::string_view enclave_name,
+      std::unique_ptr<Client::ExitCallProvider> exit_call_provider) override {
+    LoadEnclaveConfig *config =
+        reinterpret_cast<LoadEnclaveConfig *>(loader_config);
+    return LoadEnclave<SgxBackend>(enclave_name, config->base_address,
+                                   absl::GetFlag(FLAGS_enclave_binary),
+                                   config->enclave_size, config->config,
+                                   config->debug,
+                                   std::move(exit_call_provider));
+  }
+
+  // Configuration to load an instance of a SGX test enclave.
+  struct LoadEnclaveConfig {
+    void *base_address;
+    absl::string_view enclave_path;
+    size_t enclave_size;
+    EnclaveConfig config;
+    bool debug;
+  };
+};
+
+}  // namespace test
+}  // namespace primitives
+}  // namespace asylo
+
+#endif  // ASYLO_PLATFORM_PRIMITIVES_TEST_SGX_TEST_BACKEND_H_
