8d68fb31ed3bbb7f2bef1a6397d303c3873a8a61
Eric Mullen <ericmullen@google.com>
2019-06-19 10:31:51 -0700

    Implement more generic matchers for testing with protos
    PiperOrigin-RevId: 254023574
    Change-Id: I81b137bf1f1bb3e02d3eb164359802c4833ee94d

--- asylo/test/util/proto_matchers.h
+++ asylo/test/util/proto_matchers.h
@@ -21,32 +21,100 @@
 
 #include <google/protobuf/util/message_differencer.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 namespace asylo {
 
-MATCHER_P(EqualsProto, other, "") {
-  return ::google::protobuf::util::MessageDifferencer::Equals(arg, other);
+namespace internal {
+template <typename MessageT>
+class ProtoMatcher {
+ public:
+  ProtoMatcher(const MessageT &message,
+               std::function<bool(const MessageT &, const MessageT &)>
+                   message_comparator)
+      : message_(message), message_comparator_(std::move(message_comparator)) {}
+
+  void DescribeTo(std::ostream *os) const { *os << "matches"; }
+
+  void DescribeNegationTo(std::ostream *os) const { *os << "does not match"; }
+
+  bool MatchAndExplain(const MessageT &message,
+                       ::testing::MatchResultListener *listener) const {
+    if (!message_comparator_(message, message_)) {
+      *listener << "which doesn't match";
+      return false;
+    }
+    return true;
+  }
+
+ private:
+  const MessageT &message_;
+  std::function<bool(const MessageT &, const MessageT &)> message_comparator_;
+};
+
+}  // namespace internal
+
+// A proto message matches Equally to another if all fields have been
+// set to the same value.
+template <typename MessageT>
+::testing::PolymorphicMatcher<internal::ProtoMatcher<MessageT>> EqualsProto(
+    const MessageT &message) {
+  std::function<bool(const MessageT &, const MessageT &)> comparator =
+      ::google::protobuf::util::MessageDifferencer::Equals;
+  return ::testing::MakePolymorphicMatcher(
+      internal::ProtoMatcher<MessageT>(message, std::move(comparator)));
 }
 
-MATCHER_P(EquivalentProto, other, "") {
-  return ::google::protobuf::util::MessageDifferencer::Equivalent(arg, other);
+// A proto message matches Equivalently to another if all fields have
+// the same value. This is different than Equals, in that fields with
+// default values are compared. Two protos with uninitialized
+// (default) values can never be Equal, but could be Equivalent.
+template <typename MessageT>
+::testing::PolymorphicMatcher<internal::ProtoMatcher<MessageT>> EquivalentProto(
+    const MessageT &message) {
+  std::function<bool(const MessageT &, const MessageT &)> comparator =
+      ::google::protobuf::util::MessageDifferencer::Equivalent;
+  return ::testing::MakePolymorphicMatcher(
+      internal::ProtoMatcher<MessageT>(message, std::move(comparator)));
 }
 
-MATCHER_P(ApproximatelyEqualsProto, other, "") {
-  return ::google::protobuf::util::MessageDifferencer::ApproximatelyEquals(arg, other);
+// A proto message matches Approximately Equally to another if all
+// fields have been set equally, except for float fields which are
+// instead compared with MathUtil::AlmostEquals().
+template <typename MessageT>
+::testing::PolymorphicMatcher<internal::ProtoMatcher<MessageT>>
+ApproximatelyEqualsProto(const MessageT &message) {
+  std::function<bool(const MessageT &, const MessageT &)> comparator =
+      ::google::protobuf::util::MessageDifferencer::ApproximatelyEquals;
+  return ::testing::MakePolymorphicMatcher(
+      internal::ProtoMatcher<MessageT>(message, std::move(comparator)));
 }
 
-MATCHER_P(ApproximatelyEquivalentProto, other, "") {
-  return ::google::protobuf::util::MessageDifferencer::ApproximatelyEquivalent(arg,
-                                                                     other);
+// A proto message matches Approximately Equivalent to another if all
+// fields are equivalent (see EquivalentProto above), except for float
+// fields which are instead compared with MathUtil::AlmostEquals().
+template <typename MessageT>
+::testing::PolymorphicMatcher<internal::ProtoMatcher<MessageT>>
+ApproximatelyEquivalentProto(const MessageT &message) {
+  std::function<bool(const MessageT &, const MessageT &)> comparator =
+      ::google::protobuf::util::MessageDifferencer::ApproximatelyEquivalent;
+  return ::testing::MakePolymorphicMatcher(
+      internal::ProtoMatcher<MessageT>(message, std::move(comparator)));
 }
 
 // A proto message matches Partially(reference_message) if every field that is
 // set in reference_message is set to the same value in the matchee message.
-MATCHER_P(Partially, other, "") {
-  ::google::protobuf::util::MessageDifferencer differ;
-  differ.set_scope(::google::protobuf::util::MessageDifferencer::PARTIAL);
-  return differ.Compare(other, arg);
+template <typename MessageT>
+::testing::PolymorphicMatcher<internal::ProtoMatcher<MessageT>> Partially(
+    const MessageT &message) {
+  std::function<bool(const MessageT &, const MessageT &)> comparator =
+      [](const ::google::protobuf::Message &arg, const ::google::protobuf::Message &other) {
+        ::google::protobuf::util::MessageDifferencer differ;
+        differ.set_scope(::google::protobuf::util::MessageDifferencer::PARTIAL);
+        return differ.Compare(other, arg);
+      };
+  return ::testing::MakePolymorphicMatcher(
+      internal::ProtoMatcher<MessageT>(message, std::move(comparator)));
 }
 
 }  // namespace asylo
