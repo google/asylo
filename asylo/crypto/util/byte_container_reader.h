b5cc1d5da26cd84d08b41ae6b55a1c40d3fa27eb
Seth Moore <sethmo@google.com>
2019-10-30 09:00:36 -0700

    Add ByteContainerReader for reading packed data
    Helper class that consumes bytes from a ByteContainerView, copying into
    various output types.
    
    PiperOrigin-RevId: 277515017
    Change-Id: I1f02f931ae7c0c3dd0f1583764b39ee98db2d100

---
+++ asylo/crypto/util/byte_container_reader.h
@@ -0,0 +1,122 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef ASYLO_CRYPTO_UTIL_BYTE_CONTAINER_READER_H_
+#define ASYLO_CRYPTO_UTIL_BYTE_CONTAINER_READER_H_
+
+#include <openssl/mem.h>
+#include <cstring>
+#include <iterator>
+#include <type_traits>
+
+#include "absl/strings/str_format.h"
+#include "asylo/crypto/util/byte_container_view.h"
+#include "asylo/util/status.h"
+#include "asylo/util/status_macros.h"
+
+namespace asylo {
+
+// Utility class for reading from an array/stream of bytes. The intended
+// use-case is reading packed, variable-length structures in memory.
+//
+// Internally, ByteContainerReader keeps no copies of any memory, making it
+// suitable for working with secrets. It's up to the users of
+// ByteContainerReader to clean up any inputs and outputs read/written by
+// ByteContainerReader.
+class ByteContainerReader {
+ public:
+  // Create a reader that will consume bytes from |source|. The buffer
+  // referenced by |data| should have a lifetime at least as long as the
+  // constructed ByteContainerReader object.
+  explicit ByteContainerReader(ByteContainerView source)
+      : source_(source), offset_(0) {}
+
+  // Create a reader that will consume up to |size| bytes of |data|. The buffer
+  // referenced by |data| should have a lifetime at least as long as the
+  // constructed ByteContainerReader object.
+  ByteContainerReader(const void *data, size_t size)
+      : ByteContainerReader(ByteContainerView(data, size)) {}
+
+  ByteContainerReader(const ByteContainerReader &) = delete;
+  ByteContainerReader &operator=(const ByteContainerReader &) = delete;
+
+  // Returns the number of bytes still available to be read from the container.
+  size_t BytesRemaining() const { return source_.size() - offset_; }
+
+  // Read sizeof(obj) bytes into |obj|. |obj| must be trivially copy assignable.
+  template <typename ObjT>
+  Status ReadSingle(ObjT *obj) {
+    static_assert(std::is_trivially_copy_assignable<ObjT>::value,
+                  "ObjT is not trivally copy-assignable");
+    return ReadRaw(sizeof(*obj), obj);
+  }
+
+  // Append |count| objects onto |output| from the source container. Returns
+  // INVALID_ARGUMENT if |count| objects is larger than the number of bytes
+  // remaining.
+  //
+  // ByteContainerT must have a value_type that is trivially copy-assignable,
+  // and must support push_back() and back().
+  template <typename ByteContainerT>
+  Status ReadMultiple(size_t count, ByteContainerT *output) {
+    using ValueType = typename ByteContainerT::value_type;
+    static_assert(std::is_trivially_copy_assignable<ValueType>::value,
+                  "value_type is not trivally copy-assignable");
+    const size_t size = count * sizeof(ValueType);
+    if (size > BytesRemaining()) {
+      return CreateReadTooLargeStatus(size);
+    }
+
+    for (size_t i = 0; i < count; ++i) {
+      ValueType value;
+      // A failure here is "impossible" since we checked the size above.
+      ASYLO_CHECK_OK(ReadSingle(&value));
+      output->push_back(value);
+      OPENSSL_cleanse(&value, sizeof(value));
+    }
+
+    return Status::OkStatus();
+  }
+
+  // Read |size| bytes directly into the |output| buffer. Returns
+  // INVALID_ARGUMENT if |size| is larger than the number of bytes remaining.
+  Status ReadRaw(size_t size, void *output) {
+    if (size > BytesRemaining()) {
+      return CreateReadTooLargeStatus(size);
+    }
+
+    memcpy(output, source_.data() + offset_, size);
+    offset_ += size;
+    return Status::OkStatus();
+  }
+
+ private:
+  Status CreateReadTooLargeStatus(size_t size) const {
+    return Status(error::GoogleError::INVALID_ARGUMENT,
+                  absl::StrFormat(
+                      "Attempted to read %d bytes, but only %d are available",
+                      size, BytesRemaining()));
+  }
+
+  const ByteContainerView source_;
+  size_t offset_;
+};
+
+}  // namespace asylo
+
+#endif  // ASYLO_CRYPTO_UTIL_BYTE_CONTAINER_READER_H_
---
+++ asylo/crypto/util/byte_container_reader_test.cc
@@ -0,0 +1,188 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "asylo/crypto/util/byte_container_reader.h"
+
+#include <cstring>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/base/macros.h"
+#include "asylo/crypto/util/byte_container_view.h"
+#include "asylo/crypto/util/trivial_object_util.h"
+#include "asylo/test/util/status_matchers.h"
+#include "asylo/util/cleansing_types.h"
+
+namespace asylo {
+namespace {
+
+using ::testing::ElementsAreArray;
+using ::testing::Eq;
+using ::testing::Test;
+using ::testing::Types;
+
+// Test structure to serve as a trivial object type for object reads.
+struct TrivialStruct {
+  uint16_t u;
+  char c;
+  int64_t i;
+
+  bool operator==(const TrivialStruct &rhs) const {
+    return u == rhs.u && c == rhs.c && i == rhs.i;
+  }
+};
+
+template <typename T>
+class ByteContainerReadSingleTests : public Test {};
+using TrivialTypes = Types<uint8_t, int, int64_t, TrivialStruct>;
+TYPED_TEST_SUITE(ByteContainerReadSingleTests, TrivialTypes);
+
+TYPED_TEST(ByteContainerReadSingleTests, ReadOne) {
+  const TypeParam kInput = TrivialRandomObject<TypeParam>();
+  ByteContainerReader reader(ByteContainerView(&kInput, sizeof(kInput)));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(sizeof(kInput)));
+
+  TypeParam output;
+  ASSERT_THAT(reader.ReadSingle(&output), IsOk());
+  EXPECT_THAT(output, Eq(kInput));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(0));
+}
+
+TYPED_TEST(ByteContainerReadSingleTests, ReadMultiple) {
+  const TypeParam kInputs[] = {
+      TrivialRandomObject<TypeParam>(), TrivialRandomObject<TypeParam>(),
+      TrivialRandomObject<TypeParam>(), TrivialRandomObject<TypeParam>(),
+      TrivialRandomObject<TypeParam>()};
+  ByteContainerReader reader(kInputs, sizeof(kInputs));
+
+  for (size_t i = 0; i < ABSL_ARRAYSIZE(kInputs); ++i) {
+    EXPECT_THAT(reader.BytesRemaining(),
+                Eq((ABSL_ARRAYSIZE(kInputs) - i) * sizeof(TypeParam)));
+
+    TypeParam output;
+    ASSERT_THAT(reader.ReadSingle(&output), IsOk());
+    EXPECT_THAT(output, Eq(kInputs[i]));
+  }
+
+  EXPECT_THAT(reader.BytesRemaining(), Eq(0));
+}
+
+TYPED_TEST(ByteContainerReadSingleTests, ReadTooManyObjects) {
+  const TypeParam kInputs[] = {TrivialRandomObject<TypeParam>(),
+                               TrivialRandomObject<TypeParam>()};
+  ByteContainerReader reader(kInputs, sizeof(kInputs) - 1);
+
+  TypeParam output;
+  ASSERT_THAT(reader.ReadSingle(&output), IsOk());
+  EXPECT_THAT(output, Eq(kInputs[0]));
+
+  EXPECT_THAT(reader.BytesRemaining(), Eq(sizeof(TypeParam) - 1));
+
+  TypeParam *obj = nullptr;
+  ASSERT_THAT(reader.ReadSingle(obj),
+              StatusIs(error::GoogleError::INVALID_ARGUMENT));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(sizeof(TypeParam) - 1));
+}
+
+TYPED_TEST(ByteContainerReadSingleTests, ReadRaw) {
+  const TypeParam kInput = TrivialRandomObject<TypeParam>();
+  ByteContainerReader reader(ByteContainerView(&kInput, sizeof(kInput)));
+
+  TypeParam output;
+  ASSERT_THAT(reader.ReadRaw(sizeof(output), &output), IsOk());
+  EXPECT_THAT(output, Eq(kInput));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(0));
+}
+
+TYPED_TEST(ByteContainerReadSingleTests, ReadRawTooManyBytes) {
+  const TypeParam kInput = TrivialRandomObject<TypeParam>();
+  const size_t kSize = sizeof(kInput) - 1;
+  ByteContainerReader reader(ByteContainerView(&kInput, kSize));
+
+  TypeParam *output = nullptr;
+  ASSERT_THAT(reader.ReadRaw(sizeof(TypeParam), output),
+              StatusIs(error::GoogleError::INVALID_ARGUMENT));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(kSize));
+}
+
+template <typename T>
+class ByteContainerReadMultipleTests : public Test {};
+using ContainerTypes =
+    Types<std::string, CleansingString, std::vector<uint8_t>,
+          std::vector<TrivialStruct>, CleansingVector<TrivialStruct>>;
+TYPED_TEST_SUITE(ByteContainerReadMultipleTests, ContainerTypes);
+
+TYPED_TEST(ByteContainerReadMultipleTests, ReadZeroObjects) {
+  using ValueType = typename TypeParam::value_type;
+  const ValueType kInput[] = {};
+  ByteContainerReader reader(ByteContainerView(&kInput, sizeof(kInput)));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(0));
+
+  TypeParam output;
+  EXPECT_THAT(reader.ReadMultiple(0, &output), IsOk());
+  EXPECT_THAT(reader.BytesRemaining(), Eq(0));
+  EXPECT_THAT(output.size(), Eq(0));
+}
+
+TYPED_TEST(ByteContainerReadMultipleTests, ReadOneObject) {
+  using ValueType = typename TypeParam::value_type;
+  const ValueType kInput = TrivialRandomObject<ValueType>();
+  ByteContainerReader reader(ByteContainerView(&kInput, sizeof(kInput)));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(sizeof(kInput)));
+
+  TypeParam output;
+  EXPECT_THAT(reader.ReadMultiple(1, &output), IsOk());
+  EXPECT_THAT(reader.BytesRemaining(), Eq(0));
+  EXPECT_THAT(output.front(), Eq(kInput));
+}
+
+TYPED_TEST(ByteContainerReadMultipleTests, ReadMultipleObjects) {
+  using ValueType = typename TypeParam::value_type;
+  const ValueType kInput[] = {TrivialRandomObject<ValueType>(),
+                              TrivialRandomObject<ValueType>(),
+                              TrivialRandomObject<ValueType>()};
+  ByteContainerReader reader(ByteContainerView(&kInput, sizeof(kInput)));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(sizeof(kInput)));
+
+  TypeParam output;
+  EXPECT_THAT(reader.ReadMultiple(ABSL_ARRAYSIZE(kInput) - 1, &output), IsOk());
+  EXPECT_THAT(reader.BytesRemaining(), Eq(sizeof(ValueType)));
+
+  EXPECT_THAT(reader.ReadMultiple(1, &output), IsOk());
+  EXPECT_THAT(reader.BytesRemaining(), Eq(0));
+
+  EXPECT_THAT(output, ElementsAreArray(kInput));
+}
+
+TYPED_TEST(ByteContainerReadMultipleTests, ReadPastEnd) {
+  using ValueType = typename TypeParam::value_type;
+  const ValueType kInput = TrivialRandomObject<ValueType>();
+  ByteContainerReader reader(ByteContainerView(&kInput, sizeof(kInput)));
+  EXPECT_THAT(reader.BytesRemaining(), Eq(sizeof(kInput)));
+
+  TypeParam output;
+  EXPECT_THAT(reader.ReadMultiple(2, &output),
+              StatusIs(error::GoogleError::INVALID_ARGUMENT));
+
+  // Ensure that, after error, we can still read successfully
+  EXPECT_THAT(reader.ReadMultiple(1, &output), IsOk());
+  EXPECT_THAT(output.front(), Eq(kInput));
+}
+
+}  // namespace
+}  // namespace asylo
