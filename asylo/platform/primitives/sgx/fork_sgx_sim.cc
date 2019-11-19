0b566737fe90aeb0a716d71e039d128b27ac7199
Chong Cai <chongc@google.com>
2019-11-18 17:30:08 -0800

    Block enclave entries in fork in SGX sim mode
    Currently fork() in SGX sim mode can happen while other enclave threads
    are holding any locks either inside or outside the enclave. This causes
    child process to hang in multithreaded cases if other threads are
    holding locks while fork() happens. This change makes fork() wait till
    all other threads are blocked and waiting for reentering the enclave,
    and  then proceed to fork().
    The similar mechanism is already applied to fork() SGX hardware mode.
    
    PiperOrigin-RevId: 281191112
    Change-Id: Ic4b55d95b078f38b5534539aa5990c4ec8ccdc52

--- asylo/platform/primitives/sgx/fork_sgx_sim.cc
+++ asylo/platform/primitives/sgx/fork_sgx_sim.cc
@@ -16,10 +16,10 @@
  *
  */
 
-#include "asylo/platform/primitives/sgx/fork_internal.h"
-
 #include <stdlib.h>
+#include <unistd.h>
 
+#include "asylo/platform/primitives/sgx/fork_internal.h"
 #include "asylo/platform/primitives/sgx/trusted_sgx.h"
 #include "asylo/platform/primitives/trusted_runtime.h"
 #include "asylo/util/status.h"
@@ -55,8 +55,27 @@ void SetForkRequested() {
 }
 
 pid_t enc_fork(const char *enclave_name) {
-  return asylo::primitives::InvokeFork(
-      enclave_name, /*restore_snapshot=*/false);
+  // Block enclave entries while forking to make sure no other threads are
+  // holding enclave entry/exit locks during fork().
+  enc_block_entries();
+  // Confirm that all other enclave entries are blocked before proceed to
+  // fork(). All other enclave threads need to be blocked now, other than the
+  // calling thread itself.
+  // Timeout at 5 seconds.
+  constexpr int timeout = 5;
+  for (int i = 0;
+       active_entry_count() > blocked_entry_count() + 1 && i < timeout; ++i) {
+    sleep(1);
+  }
+  if (active_entry_count() > blocked_entry_count() + 1) {
+    enc_unblock_entries();
+    errno = EAGAIN;
+    return -1;
+  }
+  pid_t pid =
+      asylo::primitives::InvokeFork(enclave_name, /*restore_snapshot=*/false);
+  enc_unblock_entries();
+  return pid;
 }
 
 }  // namespace asylo
