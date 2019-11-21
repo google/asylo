63a77c9d7e4551a1dbe3f0cc94ef6954928f0ea3
Anna Sapek <asapek@google.com>
2019-11-21 10:30:57 -0800

    Add Asylo secure gRPC example main
    This change adds the enclave loaders and mains for the Asylo secure gRPC
    example.
    
    PiperOrigin-RevId: 281781075
    Change-Id: I96cbf0134b68512eab670ffc2324f03bc05b9b61

---
+++ asylo/examples/secure_grpc/grpc_client_main.cc
@@ -0,0 +1,76 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <cstdint>
+#include <string>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_cat.h"
+#include "asylo/enclave_manager.h"
+#include "asylo/examples/secure_grpc/grpc_client_util.h"
+#include "asylo/util/logging.h"
+#include "asylo/util/status.h"
+
+constexpr char kServerAddress[] = "[::1]";
+
+ABSL_FLAG(std::string, enclave_path, "", "Path to enclave to load");
+ABSL_FLAG(int32_t, port, 0, "Port that the server listens to");
+ABSL_FLAG(std::string, word_to_translate, "", "Word to be translated");
+ABSL_FLAG(bool, debug, true, "Whether to use a debug enclave");
+
+int main(int argc, char *argv[]) {
+  // Parse command-line arguments.
+  absl::ParseCommandLine(argc, argv);
+
+  std::string word_to_translate = absl::GetFlag(FLAGS_word_to_translate);
+  LOG_IF(QFATAL, word_to_translate.empty())
+      << "--word_to_translate cannot be empty";
+
+  std::string enclave_path = absl::GetFlag(FLAGS_enclave_path);
+  LOG_IF(QFATAL, enclave_path.empty()) << "--enclave_path cannot be empty";
+
+  int32_t port = absl::GetFlag(FLAGS_port);
+  LOG_IF(QFATAL, port == 0) << "--port cannot be 0";
+
+  asylo::Status status =
+      asylo::EnclaveManager::Configure(asylo::EnclaveManagerOptions());
+  LOG_IF(QFATAL, !status.ok())
+      << "Failed to configure EnclaveManager: " << status;
+
+  status = examples::secure_grpc::LoadGrpcClientEnclave(
+      enclave_path, absl::GetFlag(FLAGS_debug));
+  LOG_IF(QFATAL, !status.ok())
+      << "Loading " << enclave_path << " failed: " << status;
+
+  asylo::StatusOr<std::string> run_result =
+      examples::secure_grpc::GrpcClientEnclaveGetTranslation(
+          absl::StrCat(kServerAddress, ":", port), word_to_translate);
+  LOG_IF(QFATAL, !run_result.ok())
+      << "Getting translation for " << word_to_translate
+      << " failed: " << run_result.status();
+
+  std::cout << "Translation for \"" << word_to_translate << "\" is \""
+            << run_result.ValueOrDie() << "\"" << std::endl;
+
+  status = examples::secure_grpc::DestroyGrpcClientEnclave();
+  LOG_IF(QFATAL, !status.ok())
+      << "Destroy " << enclave_path << " failed: " << status;
+
+  return 0;
+}
---
+++ asylo/examples/secure_grpc/grpc_server_main.cc
@@ -0,0 +1,102 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+#include <google/protobuf/text_format.h>
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/time/clock.h"
+#include "asylo/enclave_manager.h"
+#include "asylo/examples/secure_grpc/grpc_server_util.h"
+#include "asylo/util/logging.h"
+#include "asylo/identity/sgx/sgx_identity.pb.h"
+#include "asylo/util/status.h"
+#include "asylo/util/statusor.h"
+
+ABSL_FLAG(std::string, enclave_path, "", "Path to enclave to load");
+
+// By default, let the server run for five minutes.
+ABSL_FLAG(int32_t, server_max_lifetime, 300,
+          "The longest amount of time (in seconds) that the server should be "
+          "allowed to run");
+
+// Default value 0 is used to indicate that the system should choose an
+// available port.
+ABSL_FLAG(int32_t, port, 0, "Port that the server listens to");
+ABSL_FLAG(bool, debug, true, "Whether to use a debug enclave");
+
+ABSL_FLAG(asylo::SgxIdentityExpectation, acl, {},
+          "The ACL to enforce within the server");
+
+namespace asylo {
+
+bool AbslParseFlag(absl::string_view text, SgxIdentityExpectation *flag,
+                   std::string *error) {
+  if (!google::protobuf::TextFormat::ParseFromString(
+          std::string(text.data(), text.size()), flag)) {
+    *error = "Failed to parse asylo::SgxIdentityExpectation";
+    return false;
+  }
+  return true;
+}
+
+std::string AbslUnparseFlag(const SgxIdentityExpectation &flag) {
+  std::string serialized_flag;
+  CHECK(google::protobuf::TextFormat::PrintToString(flag, &serialized_flag));
+  return serialized_flag;
+}
+
+}  // namespace asylo
+
+int main(int argc, char *argv[]) {
+  // Parse command-line arguments.
+  absl::ParseCommandLine(argc, argv);
+
+  std::string enclave_path = absl::GetFlag(FLAGS_enclave_path);
+  LOG_IF(QFATAL, enclave_path.empty()) << "--enclave_path cannot be empty";
+
+  asylo::Status status =
+      asylo::EnclaveManager::Configure(asylo::EnclaveManagerOptions());
+  LOG_IF(QFATAL, !status.ok())
+      << "Failed to configure EnclaveManager: " << status;
+
+  status = examples::secure_grpc::LoadGrpcServerEnclave(
+      enclave_path, absl::GetFlag(FLAGS_port), absl::GetFlag(FLAGS_acl),
+      absl::GetFlag(FLAGS_debug));
+  LOG_IF(QFATAL, !status.ok())
+      << "Loading " << enclave_path << " failed: " << status;
+
+  asylo::StatusOr<int> port_result =
+      examples::secure_grpc::GrpcServerEnclaveGetPort();
+  LOG_IF(QFATAL, !port_result.ok())
+      << "Retrieving port failed: " << port_result.status();
+
+  std::cout << "Server started on port " << port_result.ValueOrDie()
+            << std::endl;
+
+  absl::SleepFor(absl::Seconds(absl::GetFlag(FLAGS_server_max_lifetime)));
+
+  status = examples::secure_grpc::DestroyGrpcServerEnclave();
+  LOG_IF(QFATAL, !status.ok())
+      << "Destroy " << enclave_path << " failed: " << status;
+
+  return 0;
+}
