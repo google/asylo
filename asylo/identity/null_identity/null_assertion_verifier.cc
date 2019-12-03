85ca178d9825711954691a7cdacc460a573d359e
Carmen Hanish <cyhanish@google.com>
2019-12-03 14:06:54 -0800

    Move NullAssertion to internal directory
    PiperOrigin-RevId: 283618437
    Change-Id: Ie07ff7d2712974e3153fbe10655075f7e175dd57

--- asylo/identity/null_identity/null_assertion_verifier.cc
+++ asylo/identity/null_identity/null_assertion_verifier.cc
@@ -20,7 +20,7 @@
 
 #include "absl/synchronization/mutex.h"
 #include "asylo/util/logging.h"
-#include "asylo/identity/null_identity/null_assertion.pb.h"
+#include "asylo/identity/attestation/null/internal/null_assertion.pb.h"
 #include "asylo/identity/null_identity/null_identity_constants.h"
 #include "asylo/identity/null_identity/null_identity_util.h"
 #include "asylo/platform/common/static_map.h"
