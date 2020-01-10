ab8119ad643e61d22110228442b8594955c05144
Nishit Porwal <niporwal@google.com>
2020-01-09 17:55:34 -0800

    Add remaining unit tests for enclave_syscall
    PiperOrigin-RevId: 289010078
    Change-Id: If9aa989960f5e275056c0bd5cd459e63befe07b2

--- asylo/platform/posix/io/io_manager.h
+++ asylo/platform/posix/io/io_manager.h
@@ -461,30 +461,30 @@ class IOManager {
 
   // Change owner and group of a file. Returns 0 if completed successfully,
   // otherwise returns -1.
-  int Chown(const char *path, uid_t owner, gid_t group);
+  virtual int Chown(const char *path, uid_t owner, gid_t group);
 
   // Creates a hard link to an existing file. Returns 0 on success, otherwise
   // returns -1.
-  int Link(const char *from, const char *to);
+  virtual int Link(const char *from, const char *to);
 
   // Places the contents of the symbolic link |path| in the buffer |buf|.
   // Returns the number of bytes placed in buf on success, otherwise returns -1.
-  ssize_t ReadLink(const char *path, char *buf, size_t bufsize);
+  virtual ssize_t ReadLink(const char *path, char *buf, size_t bufsize);
 
   // Creates a symbolic link |to| which contains the string |from|. Returns 0 on
   // success, otherwise returns -1.
-  int SymLink(const char *from, const char *to);
+  virtual int SymLink(const char *from, const char *to);
 
   // Returns information about a file in the buffer pointed to by |stat_buffer|.
   // Returns 0 on success, otherwise returns -1. If |pathname| is a symlink,
   // returns information about the target it points to.
-  int Stat(const char *pathname, struct stat *stat_buffer);
+  virtual int Stat(const char *pathname, struct stat *stat_buffer);
 
   // Returns information about a file in the buffer pointed to by |stat_buffer|.
   // Returns 0 on success, otherwise returns -1. Unlike Stat, if |pathname| is a
   // symlink, returns information about the link itself, rather than the target
   // it points to.
-  int LStat(const char *pathname, struct stat *stat_buffer);
+  virtual int LStat(const char *pathname, struct stat *stat_buffer);
 
   // Provides a canonicalized absolute pathname that resolves symbolic links.
   // Returns |resolved_path| on success, nullptr on failure.
@@ -511,59 +511,59 @@ class IOManager {
 
   // Reads up to |count| bytes from the stream into |buf|, returning the number
   // of bytes read on success or -1 on error.
-  int Read(int fd, char *buf, size_t count);
+  virtual int Read(int fd, char *buf, size_t count);
 
   // Writes up to |count| bytes from to |fd| from |buf|, returning the number of
   // bytes written on success or -1 on error.
-  int Write(int fd, const char *buf, size_t count);
+  virtual int Write(int fd, const char *buf, size_t count);
 
   // Closes and finalizes the stream, returning 0 on success or -1 on error.
   virtual int Close(int fd) ABSL_LOCKS_EXCLUDED(fd_table_lock_);
 
   // Implements fchown(2).
-  int FChOwn(int fd, uid_t owner, gid_t group);
+  virtual int FChOwn(int fd, uid_t owner, gid_t group);
 
   // Implements lseek(2).
-  int LSeek(int fd, off_t offset, int whence);
+  virtual int LSeek(int fd, off_t offset, int whence);
 
   // Implements fchmod(2).
-  int FChMod(int fd, mode_t mode);
+  virtual int FChMod(int fd, mode_t mode);
 
   // Implements fcntl(2).
-  int FCntl(int fd, int cmd, int64_t arg);
+  virtual int FCntl(int fd, int cmd, int64_t arg);
 
   // Implements fsync(2).
-  int FSync(int fd);
+  virtual int FSync(int fd);
 
   // Implements fdatasync(2).
-  int FDataSync(int fd);
+  virtual int FDataSync(int fd);
 
   // Implements ioctl(2).
-  int Ioctl(int fd, int request, void *argp);
+  virtual int Ioctl(int fd, int request, void *argp);
 
   // Implements fstat(2).
-  int FStat(int fd, struct stat *stat_buffer);
+  virtual int FStat(int fd, struct stat *stat_buffer);
 
   // Implements statfs(2).
-  int StatFs(const char *pathname, struct statfs *statfs_buffer);
+  virtual int StatFs(const char *pathname, struct statfs *statfs_buffer);
 
   // Implements fstatfs(2).
-  int FStatFs(int fd, struct statfs *statfs_buffer);
+  virtual int FStatFs(int fd, struct statfs *statfs_buffer);
 
   // Implements isatty(3).
   int Isatty(int fd);
 
   // Implements flock(2).
-  int FLock(int fd, int operation);
+  virtual int FLock(int fd, int operation);
 
   // Implements unlink(2).
-  int Unlink(const char *pathname);
+  virtual int Unlink(const char *pathname);
 
   // Implements truncate(2).
-  int Truncate(const char *path, off_t length);
+  virtual int Truncate(const char *path, off_t length);
 
   // Implements ftruncate(2).
-  int FTruncate(int fd, off_t length);
+  virtual int FTruncate(int fd, off_t length);
 
   // Implements select(2).
   virtual int Select(int nfds, fd_set *readfds, fd_set *writefds,
@@ -584,100 +584,103 @@ class IOManager {
                         int timeout);
 
   // Implements mkdir(2).
-  int Mkdir(const char *pathname, mode_t mode);
+  virtual int Mkdir(const char *pathname, mode_t mode);
 
   // Implements rmdir(2).
-  int RmDir(const char *pathname);
+  virtual int RmDir(const char *pathname);
 
   // Implements rename(2).
-  int Rename(const char *oldpath, const char *newpath);
+  virtual int Rename(const char *oldpath, const char *newpath);
 
   // Implements writev(2).
-  ssize_t Writev(int fd, const struct iovec *iov, int iovcnt);
+  virtual ssize_t Writev(int fd, const struct iovec *iov, int iovcnt);
 
   // Implements readv(2).
-  ssize_t Readv(int fd, const struct iovec *iov, int iovcnt);
+  virtual ssize_t Readv(int fd, const struct iovec *iov, int iovcnt);
 
   // Implements pread(2).
-  ssize_t PRead(int fd, void *buf, size_t count, off_t offset);
+  virtual ssize_t PRead(int fd, void *buf, size_t count, off_t offset);
 
   // Implements umask(2).
-  mode_t Umask(mode_t mask);
+  virtual mode_t Umask(mode_t mask);
 
   // Implements chmod(2).
-  int ChMod(const char *pathname, mode_t mode);
+  virtual int ChMod(const char *pathname, mode_t mode);
 
   // Implements utime(2).
-  int Utime(const char *filename, const struct utimbuf *times);
+  virtual int Utime(const char *filename, const struct utimbuf *times);
 
   // Implements utimes(2).
-  int Utimes(const char *filename, const struct timeval times[2]);
+  virtual int Utimes(const char *filename, const struct timeval times[2]);
 
   // Implements getrlimit(2).
-  int GetRLimit(int resource, struct rlimit *rlim)
+  virtual int GetRLimit(int resource, struct rlimit *rlim)
       ABSL_LOCKS_EXCLUDED(fd_table_lock_);
 
   // Implements setrlimit(2).
-  int SetRLimit(int resource, const struct rlimit *rlim)
+  virtual int SetRLimit(int resource, const struct rlimit *rlim)
       ABSL_LOCKS_EXCLUDED(fd_table_lock_);
 
   // Implements setsockopt(2).
-  int SetSockOpt(int sockfd, int level, int option_name,
-                 const void *option_value, socklen_t option_len);
+  virtual int SetSockOpt(int sockfd, int level, int option_name,
+                         const void *option_value, socklen_t option_len);
 
   // Implements connect(2).
-  int Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+  virtual int Connect(int sockfd, const struct sockaddr *addr,
+                      socklen_t addrlen);
 
   // Implements shutdown(2).
-  int Shutdown(int sockfd, int how);
+  virtual int Shutdown(int sockfd, int how);
 
   // Implements send(2).
-  ssize_t Send(int sockfd, const void *buf, size_t len, int flags);
+  virtual ssize_t Send(int sockfd, const void *buf, size_t len, int flags);
 
   // Implements getsockopt(2).
-  int GetSockOpt(int sockfd, int level, int optname, void *optval,
-                 socklen_t *optlen);
+  virtual int GetSockOpt(int sockfd, int level, int optname, void *optval,
+                         socklen_t *optlen);
 
   // Implements accept(2).
-  int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+  virtual int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 
   // Implements bind(2).
-  int Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+  virtual int Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 
   // Implements listen(2).
-  int Listen(int sockfd, int backlog);
+  virtual int Listen(int sockfd, int backlog);
 
   // Implements sendmsg(2).
-  ssize_t SendMsg(int sockfd, const struct msghdr *msg, int flags);
+  virtual ssize_t SendMsg(int sockfd, const struct msghdr *msg, int flags);
 
   // Implements recvmsg(2).
-  ssize_t RecvMsg(int sockfd, struct msghdr *msg, int flags);
+  virtual ssize_t RecvMsg(int sockfd, struct msghdr *msg, int flags);
 
   // Implements getsockname(2).
-  int GetSockName(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+  virtual int GetSockName(int sockfd, struct sockaddr *addr,
+                          socklen_t *addrlen);
 
   // Implements getpeername(2).
-  int GetPeerName(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+  virtual int GetPeerName(int sockfd, struct sockaddr *addr,
+                          socklen_t *addrlen);
 
   // Implements socket(2).
-  int Socket(int domain, int type, int protocol);
+  virtual int Socket(int domain, int type, int protocol);
 
   // Implements eventfd(2).
-  int EventFd(unsigned int initval, int flags)
+  virtual int EventFd(unsigned int initval, int flags)
       ABSL_LOCKS_EXCLUDED(fd_table_lock_);
 
   // Implements inotify_init(2).
-  int InotifyInit(bool non_block) ABSL_LOCKS_EXCLUDED(fd_table_lock_);
+  virtual int InotifyInit(bool non_block) ABSL_LOCKS_EXCLUDED(fd_table_lock_);
 
   // Implements inotify_add_watch(2).
-  int InotifyAddWatch(int fd, const char *pathname, uint32_t mask);
+  virtual int InotifyAddWatch(int fd, const char *pathname, uint32_t mask);
 
   // Implements inotify_rm_watch(2).
-  int InotifyRmWatch(int fd, int wd);
+  virtual int InotifyRmWatch(int fd, int wd);
 
   // Implements recvfrom(2).
-  ssize_t RecvFrom(int sockfd, void *buf, size_t len, int flags,
-                   struct sockaddr *src_addr, socklen_t *addrlen);
+  virtual ssize_t RecvFrom(int sockfd, void *buf, size_t len, int flags,
+                           struct sockaddr *src_addr, socklen_t *addrlen);
   // Binds an enclave file descriptor to a host file descriptor, returning an
   // enclave file descriptor which will delegate all I/O operations to the host
   // operating system.
