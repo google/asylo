20336ae79977acc608d16a972aa236376e734ab6
Chong Cai <chongc@google.com>
2020-04-02 13:30:24 -0700

    Check buffer size in DeserializePasswd
    PiperOrigin-RevId: 304466096
    Change-Id: I9cbb7a1e6321c7a332995fda23404a77f67cace9

--- asylo/platform/host_call/trusted/host_calls.cc
+++ asylo/platform/host_call/trusted/host_calls.cc
@@ -107,16 +107,18 @@ bool DeserializePasswd(MessageReader *reader,
   auto pw_dir_buf = reader->next();
   auto pw_shell_buf = reader->next();
 
-  strncpy(passwd_buffers->pw_name, pw_name_buf.As<char>(), pw_name_buf.size());
+  strncpy(passwd_buffers->pw_name, pw_name_buf.As<char>(),
+          std::min(sizeof(passwd_buffers->pw_name), pw_name_buf.size()));
   strncpy(passwd_buffers->pw_passwd, pw_passwd_buf.As<char>(),
-          pw_passwd_buf.size());
+          std::min(sizeof(passwd_buffers->pw_passwd), pw_passwd_buf.size()));
   passwd_buffers->pw_uid = pw_uid;
   passwd_buffers->pw_gid = pw_gid;
   strncpy(passwd_buffers->pw_gecos, pw_gecos_buf.As<char>(),
-          pw_gecos_buf.size());
-  strncpy(passwd_buffers->pw_dir, pw_dir_buf.As<char>(), pw_dir_buf.size());
+          std::min(sizeof(passwd_buffers->pw_gecos), pw_gecos_buf.size()));
+  strncpy(passwd_buffers->pw_dir, pw_dir_buf.As<char>(),
+          std::min(sizeof(passwd_buffers->pw_dir), pw_dir_buf.size()));
   strncpy(passwd_buffers->pw_shell, pw_shell_buf.As<char>(),
-          pw_shell_buf.size());
+          std::min(sizeof(passwd_buffers->pw_shell), pw_shell_buf.size()));
   return true;
 }
 
