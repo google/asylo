43736823ed978e1c2363f9f80328d5eacec86f74
Seth Moore <sethmo@google.com>
2020-07-06 10:43:13 -0700

    Add e2e QE generator and verifier test
    Extend the Intel ECDSA QE remote assertion generator end-to-end test
    with real verification. This way, the test can perform true,
    end-to-end verification of the QE-based assertion support.
    
    PiperOrigin-RevId: 319809609
    Change-Id: I5c30bdf9b837b1ebc80c03a7795ab66922116f4a

--- asylo/identity/attestation/sgx/sgx_intel_ecdsa_qe_remote_assertion_e2e_test.cc
+++ asylo/identity/attestation/sgx/sgx_intel_ecdsa_qe_remote_assertion_e2e_test.cc
@@ -16,21 +16,37 @@
  *
  */
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <vector>
+
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/text_format.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/flags/flag.h"
+#include "asylo/crypto/certificate.pb.h"
 #include "asylo/identity/attestation/sgx/internal/dcap_intel_architectural_enclave_path_setter.h"
+#include "asylo/identity/attestation/sgx/internal/intel_certs/qe_identity.h"
+#include "asylo/identity/attestation/sgx/sgx_intel_ecdsa_qe_remote_assertion_verifier.h"
 #include "asylo/identity/attestation/sgx/sgx_remote_assertion_generator_test_enclave_wrapper.h"
 #include "asylo/identity/descriptions.h"
 #include "asylo/identity/enclave_assertion_authority_config.pb.h"
 #include "asylo/identity/enclave_assertion_authority_configs.h"
+#include "asylo/identity/identity.pb.h"
 #include "asylo/platform/core/enclave_manager.h"
 #include "asylo/test/util/proto_matchers.h"
 #include "asylo/test/util/status_matchers.h"
+#include "asylo/util/proto_parse_util.h"
 #include "asylo/util/status_macros.h"
 
 ABSL_FLAG(std::string, generator_test_enclave_path, "",
           "Path to the generator test enclave");
+
+ABSL_FLAG(std::string, pck_cert_chain_path, "",
+          "Path to the PEM-encoded PCK certificate chain for this platform");
 
 namespace asylo {
 namespace {
@@ -53,17 +69,38 @@ class SgxIntelEcdsaQeRemoteAssertionE2eTest : public Test {
   static void TearDownTestSuite() { delete test_enclave_; }
 
   void SetUp() override {
-    EnclaveAssertionAuthorityConfig config;
-    ASYLO_ASSERT_OK_AND_ASSIGN(
-        config,
-        experimental::CreateSgxIntelEcdsaQeRemoteAssertionAuthorityConfig());
+    std::vector<Certificate> certs;
+    std::string path = absl::GetFlag(FLAGS_pck_cert_chain_path);
+    if (path.empty()) {
+      ASYLO_ASSERT_OK_AND_ASSIGN(
+          assertion_authority_config_,
+          experimental::CreateSgxIntelEcdsaQeRemoteAssertionAuthorityConfig());
+    } else {
+      int fd = open(path.c_str(), O_RDONLY);
+      ASSERT_NE(fd, -1) << strerror(errno);
+
+      google::protobuf::io::FileInputStream input(fd);
+      input.SetCloseOnDelete(true);
+
+      CertificateChain cert_chain_proto;
+      ASSERT_TRUE(google::protobuf::TextFormat::Parse(&input, &cert_chain_proto));
+      certs = {cert_chain_proto.certificates().begin(),
+               cert_chain_proto.certificates().end()};
+
+      ASYLO_ASSERT_OK_AND_ASSIGN(
+          assertion_authority_config_,
+          experimental::CreateSgxIntelEcdsaQeRemoteAssertionAuthorityConfig(
+              certs, ParseTextProtoOrDie(sgx::kIntelEcdsaQeIdentityTextproto)));
+    }
 
     ASYLO_ASSERT_OK(test_enclave_->ResetGenerator());
-    ASYLO_ASSERT_OK(test_enclave_->Initialize(config.config()));
+    ASYLO_ASSERT_OK(
+        test_enclave_->Initialize(assertion_authority_config_.config()));
   }
 
   static EnclaveManager *enclave_manager_;
   static SgxRemoteAssertionGeneratorTestEnclaveWrapper *test_enclave_;
+  EnclaveAssertionAuthorityConfig assertion_authority_config_;
 
  private:
   static Status LoadTestEnclave() {
@@ -87,6 +124,15 @@ EnclaveManager *SgxIntelEcdsaQeRemoteAssertionE2eTest::enclave_manager_;
 SgxRemoteAssertionGeneratorTestEnclaveWrapper
     *SgxIntelEcdsaQeRemoteAssertionE2eTest::test_enclave_;
 
+TEST_F(SgxIntelEcdsaQeRemoteAssertionE2eTest, VerifyAssertionOffer) {
+  AssertionOffer offer;
+  ASYLO_ASSERT_OK_AND_ASSIGN(offer, test_enclave_->CreateAssertionOffer());
+
+  SgxIntelEcdsaQeRemoteAssertionVerifier verifier;
+  ASYLO_ASSERT_OK(verifier.Initialize(assertion_authority_config_.config()));
+  EXPECT_THAT(verifier.CanVerify(offer), IsOkAndHolds(true));
+}
+
 TEST_F(SgxIntelEcdsaQeRemoteAssertionE2eTest, VerifyAssertionGeneration) {
   constexpr char kUserData[] = "This is some user data";
   AssertionRequest request;
@@ -97,8 +143,20 @@ TEST_F(SgxIntelEcdsaQeRemoteAssertionE2eTest, VerifyAssertionGeneration) {
                              test_enclave_->Generate(kUserData, request));
 
   EXPECT_THAT(assertion.description(), EqualsProto(request.description()));
-
   EXPECT_GT(assertion.assertion().size(), 0);
+
+  SgxIntelEcdsaQeRemoteAssertionVerifier verifier;
+  ASYLO_ASSERT_OK(verifier.Initialize(assertion_authority_config_.config()));
+
+  if (absl::GetFlag(FLAGS_pck_cert_chain_path).empty()) {
+    LOG(INFO) << "The flag " << FLAGS_pck_cert_chain_path.Name()
+              << " was not set, so the test is skipping quote verification.";
+  } else {
+    LOG(INFO) << "Verifying quote with cert chain from "
+              << absl::GetFlag(FLAGS_pck_cert_chain_path);
+    EnclaveIdentity peer_identity;
+    ASYLO_ASSERT_OK(verifier.Verify(kUserData, assertion, &peer_identity));
+  }
 }
 
 }  // namespace
