fc38a0f29b6321a526354371efb9873ff14bcf40
Chong Cai <chongc@google.com>
2021-01-11 15:52:31 -0800

    Add wait4 declaration
    PiperOrigin-RevId: 351253473
    Change-Id: If6b48cebc906e85ce7f3f06146df94c8573ef460

--- asylo/platform/posix/include/sys/wait.h
+++ asylo/platform/posix/include/sys/wait.h
@@ -30,6 +30,7 @@ extern "C" {
 #define WNOHANG 1
 
 pid_t wait3(int *wstatus, int options, struct rusage *rusage);
+pid_t wait4(pid_t pid, int *wstatus, int options, struct rusage *rusage);
 
 #ifdef __cplusplus
 }  // extern "C"
