f5c4688bee65700e5f9db0efdb714681e9b5ec4c
Conrad Parker <conradparker@google.com>
2021-03-22 12:39:11 -0700

    Remove EkepErrorSpace
    This change also alters the way that EkepError() embeds Abort codes in
    Statuses. This alteration is an implementation detail of EkepError().
    
    Since EkepErrorSpace is entirely internal to Asylo, it doesn't need a
    deprecation period.
    
    PiperOrigin-RevId: 364384181
    Change-Id: If733848af9bdf228ee705d11212e8608a555c96f

--- asylo/grpc/auth/core/ekep_errors.cc
+++ asylo/grpc/auth/core/ekep_errors.cc
@@ -16,23 +16,39 @@
 
 #include "asylo/grpc/auth/core/ekep_errors.h"
 
+#include <string>
+
+#include "absl/status/status.h"
+#include "absl/strings/cord.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
-#include "asylo/grpc/auth/core/ekep_error_space.h"
 #include "asylo/grpc/auth/core/handshake.pb.h"
 #include "asylo/util/status.h"
+#include "asylo/util/status_helpers.h"
 
 namespace asylo {
 
 Status EkepError(Abort_ErrorCode code, absl::string_view message) {
-  return Status(code, message);
+  Status status = absl::InternalError(message);
+  Abort abort;
+  abort.set_code(code);
+  status.SetPayload(GetTypeUrl<Abort>(), absl::Cord(abort.SerializeAsString()));
+  return status;
 }
 
 absl::optional<Abort_ErrorCode> GetEkepErrorCode(const Status &status) {
-  if (status.error_space() == EkepErrorSpace::GetInstance()) {
-    return static_cast<Abort_ErrorCode>(status.raw_code());
+  absl::optional<absl::Cord> payload = status.GetPayload(GetTypeUrl<Abort>());
+  if (!payload.has_value()) {
+    return absl::nullopt;
   }
-  return absl::nullopt;
+  Abort abort;
+  if (!abort.ParseFromString(std::string(payload.value()))) {
+    // This indicates that an Abort code exists but its value could not be
+    // determined.
+    return Abort::UNKNOWN_ERROR_CODE;
+  }
+  return abort.code();
 }
 
 }  // namespace asylo
